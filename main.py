ed467f2c2e74ba2b7b2e4e951ec74eccb2dd3dd9
Gabriel Oliveira <155106088+gabriel-oligom@users.noreply.github.com>
2025-08-22 16:35:14 -0300

    Implement datetime and organize email body from API

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
 import requests
+from datetime import datetime, timedelta
+
+# Put the date time of today
+today = datetime.now()
+
+# Convert the number of the day that we got to a string in the URL
+#today.strftime("%Y-%m-%d") (just a test)
+
+# Includes the last 7 days, putting today less the seven days with timedelta
+seven_days_before = today - timedelta(days=7)
+
+# Convert the number of the seven days that we got to a string in the URL
+api_date = seven_days_before.strftime("%Y-%m-%d")
+
 api_key = "78721f510cf549659a101e33ea091825"
-url = "https://newsapi.org/v2/everything?q=tesla&from=2025-07-21&" \
+url = f"https://newsapi.org/v2/everything?q=tesla&from={api_date}&" \
 "sortBy=publishedAt&apiKey=78721f510cf549659a101e33ea091825"
 
 # Make a request
@@ -9,7 +23,13 @@ request = requests.get(url)
 # Create a dictionary/list with JSON
 content = request.json()
 
-# Access the article titles and descriptions with a loop that goes through all the articles
-for article in content["articles"]:
-    print(article["title"])
-    print(article["descriptions"])
+email_body = ""
+
+# Access the article titles, descriptions and url with a loop that 
+# goes through all the articles, and enumerate them to organize
+for i, article in enumerate(content["articles"], 1):
+    title = article.get("title", "(no title)")
+    description = article.get("description", "(no description)")
+    url = article.get("url", "")
+
+    email_body += f"{i}. {title}\n {description}\n {url}\n"
